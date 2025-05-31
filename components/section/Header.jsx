b841d13060f52034250acf3de636f575177b2eb3
swapnilsharm881 <swapnilsharma881@gmail.com>
2025-05-31 22:11:23 +0530

    fixed portfolio conflict

--- components/section/Header.jsx
+++ components/section/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
           <Link href="/" onClick={() => setIsOpen(false)}>
             <span className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Home</span>
           </Link>
-          <Link href="/Portfolio" onClick={() => setIsOpen(false)}>
+          <Link href="/portfolio" onClick={() => setIsOpen(false)}>
             <span className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Portfolio</span>
           </Link>
           <Link href="/contact" onClick={() => setIsOpen(false)}>
