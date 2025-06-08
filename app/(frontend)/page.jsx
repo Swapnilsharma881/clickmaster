35ce638d47b569a2434eafafe7ea5907445186df
swapnilsharm881 <swapnilsharma881@gmail.com>
2025-06-08 22:18:11 +0530

    added metadata for home page

--- app/(frontend)/page.jsx
+++ app/(frontend)/page.jsx
@@ -1,18 +1,20 @@
 import HeroSection from "@/section/HeroSection";
-import FeaturedWork from "@/section/FeaturedWork"
-import MeetTheMan from "@/section/MeetTheMan"
-import BrandMarquee from "@/section/BrandMarquee";
+import FeaturedWork from "@/section/FeaturedWork";
+import MeetTheMan from "@/section/MeetTheMan";
 
-const Home =  () => {
+export const metadata = {
+  title: "Clickkmaster | Freelance Photographer in North India",
+  description: "Freelance photographer offering , event, and portrait photography in Delhi, Jaipur, Lucknow, and across North India.",
+};
 
-
+const Home = () => {
   return (
-    <>
-    <HeroSection/>
-    <FeaturedWork/>
-    <MeetTheMan/>
-    </>
-  )
-}
+    <main>
+      <HeroSection />
+      <FeaturedWork />
+      <MeetTheMan />
+    </main>
+  );
+};
 
 export default Home;
